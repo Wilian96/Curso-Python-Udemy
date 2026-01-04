0d58b19dff39fe7299b7c47d186cc150f54b1d70
Wilian96 <64976355+Wilian96@users.noreply.github.com>
2026-01-04 18:10:49 -0300

    Digitei o Código de FStrings.

---
+++ FStrings/FStrings.py
@@ -0,0 +1,8 @@
+nome = "João"
+Sobrenome = "Silva"
+peso = 85.5
+altura = 1.75
+imc = peso / (altura ** 2)
+
+print(f"{nome} {Sobrenome} tem {altura:.2f} de altura e pesa {peso} quilos.")
+print(f"O IMC de {nome} é {imc:.2f}.")
